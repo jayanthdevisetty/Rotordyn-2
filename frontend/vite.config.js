782bfbf49d13d96d8714dd933623ca1871820855
jayanthdevisetty <djay8im@gmail.com>
2026-07-16 15:30:30 +0000

    Configure sourcemap false and minify true in vite.config.js to disable source maps in production build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: true,
+    cssMinify: true
+  },
   server: {
     port: 5000,
     host: true
